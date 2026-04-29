23c3b257fd51ba86a7747d4ac47fbce1a67f5f8c
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-04-29 14:15:02 +0530

    file creation and define entity

---
+++ src/main/java/com/CarPooling/CarPoolingPlatform/CarPoolingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.CarPooling.CarPoolingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPoolingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPoolingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CarPooling/CarPoolingPlatform/CarPoolingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CarPooling.CarPoolingPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPoolingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
