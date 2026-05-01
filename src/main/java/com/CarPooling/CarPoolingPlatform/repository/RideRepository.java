18a5ee0b90b3a626974353435ca27df38031d6d0
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-05-01 21:05:52 +0530

    Ride Booking, jwt authentication creation

---
+++ src/main/java/com/CarPooling/CarPoolingPlatform/repository/RideRepository.java
@@ -0,0 +1,14 @@
+package com.CarPooling.CarPoolingPlatform.repository;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.CarPooling.CarPoolingPlatform.entity.Ride;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface RideRepository extends JpaRepository<Ride,  Long> {
+    List<Ride> findBySourceIgnoreCaseAndDestinationIgnoreCase(String source, String destination);
+
+    @Query("SELECT r FROM Ride r WHERE LOWER(r.source) LIKE LOWER(CONCAT('%', :source, '%')) AND LOWER(r.destination) LIKE LOWER(CONCAT('%', :destination, '%'))")
+    List<Ride> searchPartial(@Param("source") String source,
+                             @Param("destination") String destination);
+}
--- src/main/java/com/CarPooling/CarPoolingPlatform/security/JwtUtil.java
+++ src/main/java/com/CarPooling/CarPoolingPlatform/security/JwtUtil.java
@@ -30,4 +30,22 @@ public class JwtUtil {
                 .getBody()
                 .getSubject();
     }
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
