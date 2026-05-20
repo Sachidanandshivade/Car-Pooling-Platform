7254991d2456b41617e649024ba9d990684b4ebc
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-05-21 00:09:43 +0530

    only backend files

--- frontend/src/services/authService.js
+++
@@ -1,9 +0,0 @@
-import api from "../api/axiosConfig";
-
-export const registerUser = async (data) => {
-    return await api.post("/auth/register", data);
-};
-
-export const loginUser = async (data) => {
-    return await api.post("/auth/login", data);
-};
