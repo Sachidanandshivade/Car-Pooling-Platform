31204e3ee36f78fea76e29ee1ec098c2a80760ea
sachidanand <114554207+Sachidanandshivade@users.noreply.github.com>
2026-05-22 14:59:09 +0530

    Refactor RideRequestService to use LocationService for distance
    Removed hardcoded fare constants and replaced distance calculation with locationService method.

--- backend/src/main/java/com/CarPooling/CarPoolingPlatform/service/RideRequestService.java
+++ backend/src/main/java/com/CarPooling/CarPoolingPlatform/service/RideRequestService.java
@@ -18,10 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RideRequestService {
 
-      private static final double BASE_FARE = 30;
-    private static final double PRICE_PER_KM = 10;
-      System.out.println("SOURCE LAT LNG: " + request.getSourceLat() + " , " + request.getSourceLng());
-System.out.println("DEST LAT LNG: " + request.getDestLat() + " , " + request.getDestLng());
+      private final LocationService locationService;
     
     private final LocationService locationService;
     private final RideRequestRepository rideRequestRepository;
@@ -68,13 +65,12 @@ System.out.println("DEST LAT LNG: " + request.getDestLat() + " , " + request.get
                 throw new RuntimeException("You are already on a ride");
             }
             
-            double distance = calculateDistance(
+            double distance = locationService.distance(
         request.getSourceLat(),
         request.getSourceLng(),
         request.getDestLat(),
         request.getDestLng()
 );
-
 double fare = BASE_FARE + (distance * PRICE_PER_KM);
 
             Ride newRide = Ride.builder()
@@ -111,18 +107,5 @@ double fare = BASE_FARE + (distance * PRICE_PER_KM);
                 .orElseThrow(() -> new RuntimeException("User not found"));
         return rideRequestRepository.findByPassenger(passenger);
     }
-    private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-    final int R = 6371;
-
-    double latDistance = Math.toRadians(lat2 - lat1);
-    double lonDistance = Math.toRadians(lon2 - lon1);
-
-    double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-            + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-            * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-
-    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    return R * c;
-}
+    
 }
