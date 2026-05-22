419907ed6c3b02a0fad7302cb50335029e271958
sachidanand <114554207+Sachidanandshivade@users.noreply.github.com>
2026-05-22 17:09:12 +0530

    Add LocationService to RideRequestController

--- backend/src/main/java/com/CarPooling/CarPoolingPlatform/controller/RideRequestController.java
+++ backend/src/main/java/com/CarPooling/CarPoolingPlatform/controller/RideRequestController.java
@@ -18,6 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RideRequestController {
 
+     private final LocationService locationService;
     private final RideRequestService rideRequestService;
 
     @PostMapping
