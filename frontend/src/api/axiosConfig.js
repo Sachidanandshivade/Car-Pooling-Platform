5fa6968b28bbc921fa0430e0d5bef22fa1c8e4dd
sachidanand <114554207+Sachidanandshivade@users.noreply.github.com>
2026-05-22 10:00:53 +0530

    Update API base URL to production environment

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://10.116.165.138:8080",
+    baseURL: "https://car-pooling-platform-3.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
